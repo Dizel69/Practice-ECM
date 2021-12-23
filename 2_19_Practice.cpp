982bdbd81885b75c3eeef9389acb8512ac2816ab
Dizel69 <dizelking@yandex.ru>
2021-12-23 17:25:46 +0300

    Передал pow на другую функцию

--- 2_19_Practice.cpp
+++ 2_19_Practice.cpp
@@ -2,31 +2,23 @@
 //         Дана  целочисленная  квадратная  матрица  размером  n*n.  Вычислить (x1+xn)*(x2+xn-1)* …*(xn+x1),
 //         где  xk  -  среднее  геометрическое  из  элементов  k-й  строки  данной  матрицы.
 
-#include<iostream>
-#include<math.h>
+#include <iostream>
+
 
 int** Get_Matr(int n);
 void Fill_Matr(int** a, int n);
 void Remove_Matr(int** matr, int n);
 int Geo_Mean(int* arr, int n);
+int my_pow(int res, int n);
+int MPNH(int** matr, int n);
 
 int main(){
-	int n;
-	printf("Enter n: ");
-	scanf_s("%d", &n);
-
-	int res = 1;
+	int n = 4;
 	int** matr = Get_Matr(n);
 
 	Fill_Matr(matr, n);
 
-	int k = n;
-	for (int i = 0; i < n; i++){
-		res *= Geo_Mean(matr[i], n) + Geo_Mean(matr[k - 1], n);
-		k--;
-	}
-
-	printf("%d", res);
+	printf("%d", MPNH(matr, n));
 
 	Remove_Matr(matr, n);
 
@@ -58,8 +50,31 @@ void Remove_Matr(int** matr, int n){
 
 int Geo_Mean(int* arr, int n){
 	int res = 1;
+
 	for (int i = 0; i < n; i++)
 		res *= arr[i];
 
-	return pow(res, (1.0 / n));
+	return my_pow(res, n);
+}
+
+int my_pow(int res, int n){
+	int result = 1, z = 1;
+
+	for (int i = 0; i < n; i++){
+		result = result * z;
+
+		if (result == res)
+			return z;
+		else
+			z++;
+	}
+}
+
+int MPNH(int** matr, int n){
+	int res = 1, k = n;
+	for (int i = 0; i < n; i++){
+		res *= Geo_Mean(matr[i], n) + Geo_Mean(matr[k - 1], n);
+		k--;
+	}
+	return res;
 }
